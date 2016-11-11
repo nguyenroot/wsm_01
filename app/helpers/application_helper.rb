5e7f9bf1f2b2d383204a970a6988679fc3267101
nguyenroot <tranthaonguyen.dsher@gmail.com>
2016-11-11 18:28:12 +0700

    CRUD_Project

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,9 @@ module ApplicationHelper
       page_title + " | " + base_title
     end
   end
+
+  def paginate objects, options = {}
+    options.reverse_merge! theme: "twitter-bootstrap-3"
+    super objects, options
+  end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,10 @@
 class Project < ApplicationRecord
   has_many :project_members
   has_many :users, through: :project_members
+
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validates :name, presence: true, length: Settings.maximum.length
+  validates :abbreviation, presence: true, length: Settings.maximum.length
+  scope :newest, ->{order created_at: :desc}
 end
