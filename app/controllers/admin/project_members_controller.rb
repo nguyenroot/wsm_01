95e4d9b5a5ca26f3baf94d591c600f83b8cc0633
nguyenroot <tranthaonguyen.dsher@gmail.com>
2016-11-14 17:12:16 +0700

    CRUD_Project_member

--- app/controllers/admin/project_members_controller.rb
+++ app/controllers/admin/project_members_controller.rb
@@ -1,6 +1,8 @@
 class Admin::ProjectMembersController < ApplicationController
+  before_action :load_project, except: [:index, :new, :create]
 
   def index
+    @users = User.all
     @projects = Project.all
     @project_members = ProjectMember.page(params[:page])
       .per Settings.pagination.per_page
@@ -10,6 +12,11 @@ class Admin::ProjectMembersController < ApplicationController
     @users= User.all
     @projects = Project.all
     @project_member = ProjectMember.new
+  end
+
+  def edit
+    @users = User.all
+    @projects = Project.all
   end
 
   def create
@@ -26,15 +33,32 @@ class Admin::ProjectMembersController < ApplicationController
   def update
     if @project_member.update_attributes project_member_params
       flash.now[:success] = t "flash.success.updated_project_member"
-      redirect_to admin_project_member_path
+      redirect_to admin_project_members_path
     else
       flash.now[:danger] = t "flash.danger.updated_project_members"
       render :edit
     end
   end
 
+  def destroy
+    if @project_member.destroy
+      flash.now[:success] = t "flash.success.deleted_project"
+    else
+      flash.now[:danger] = t "flash.danger.deleted_project"
+    end
+    redirect_to admin_project_members_path
+  end
+
   private
   def project_member_params
     params.require(:project_member).permit :user_id, :project_id, :project_role
+  end
+
+  def load_project
+    @project_member = ProjectMember.find_by id: params[:id]
+    unless @project_member
+      flash.now[:danger] = t "flash.danger.project_not_found"
+      redirect_to admin_project_members_path
+    end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-2.times do |n|
+20.times do |n|
   name = Faker::Name.name
   email = "trainee-#{n+1}@gmail.com"
   gender = "1"
